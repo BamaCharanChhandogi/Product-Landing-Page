14e64cc17d6975ffe438871c5f062f9e709a680d
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-05-29 01:41:48 +0530

    update

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,4 @@
+// Header page navigation bar
 let btn=document.getElementById('bar');
 let navbar=document.getElementById('navbar');
 let close=document.getElementById('close');
@@ -13,33 +14,83 @@ if(close){
         document.getElementById('close').style.display="none";
     });
 }
-//
-// window.addEventListener('scroll',function(){
-//     if(window.pageYOffset>=820 && window.pageYOffset <= 3500){
-//         document.getElementById('product').classList.add('active');
-//         document.getElementById('home').classList.remove('active');
-//         document.getElementById('about').classList.remove('active');
-//     }
-//     else if(window.pageYOffset>3500){
-//         document.getElementById('about').classList.add('active');
-//         document.getElementById('product').classList.remove('active');
-//         document.getElementById('home').classList.remove('active');
-//     }
-//     else{
-//         document.getElementById('product').classList.remove('active');
-//         document.getElementById('home').classList.remove('active');
-//         document.getElementById('home').classList.add('active');
-//     }
-// });
-
+// Shop page 
 // button working
-let value=document.getElementById('value');
 let count=0;
-let button=document.querySelectorAll('.btnShop');
-button.forEach(btnShop =>{
-    btnShop.addEventListener('click',function(){
-        count=count+1; 
-        btnShop.setAttribute('style','color:green;');
-    });
+document.querySelectorAll(".btnShop").forEach(box => 
+    box.addEventListener("click", () =>{
+        document.getElementById('cart-item').style.display='block';
+        box.style.color='green';
+        count++;
+        document.getElementById('cart-item').innerHTML=count;
+
+    })
+  )
+
+let input =document.getElementById('input');
+let couponOff=document.getElementById('offer');  // Total value in cart
+let final=document.getElementById('final-value');  // subtotal value in cart 
+//for update product-1 quantity
+document.getElementById('productN1').addEventListener('click',()=>{
+    let a=document.getElementById('productN1').value;
+    let b=document.getElementById('update1').innerText; // subtotal list
+    let update=a*b;
+    document.getElementById('update1').innerHTML=update;
+    final.innerHTML=parseInt(update)+parseInt(final.innerHTML);
+    couponOff.innerHTML=final.innerHTML;
 });
-//update on shopping page
+//for update product-2 quantity
+document.getElementById('productN2').addEventListener('click',()=>{
+    let a=document.getElementById('productN2').value;
+    let b=document.getElementById('update2').innerText;
+    let update=a*b;
+    document.getElementById('update2').innerHTML=update;
+    final.innerHTML=parseInt(update)+parseInt(final.innerHTML);
+    couponOff.innerHTML=final.innerHTML;
+});
+//for update product-3 quantity
+document.getElementById('productN3').addEventListener('click',()=>{
+    let a=document.getElementById('productN3').value;
+    let b=document.getElementById('update3').innerText;
+    let update=a*b;
+    document.getElementById('update3').innerHTML=update;
+    final.innerHTML=parseInt(update)+parseInt(final.innerHTML);
+    couponOff.innerHTML=final.innerHTML;
+});
+
+//for coupon code
+document.getElementById('normal').addEventListener('click',()=>{
+    if(input.value=='#FEE'){
+        alert('Congratulations! You got 70% off');
+        couponOff.innerText='910';
+        document.getElementById('sp-coupon').style.display='contents';
+    }
+    else{
+        alert('Invalid Code');
+    }
+});
+
+// cart page
+
+function nth1(){
+    document.getElementById('nth1').style.display="none";
+    let a=parseInt(document.getElementById('update1').innerHTML);
+    let b=parseInt(final.innerHTML);
+    document.getElementById('final-value').innerHTML=b-a;
+    couponOff.innerHTML=final.innerHTML;
+
+}
+function nth2(){
+    document.getElementById('nth2').style.display="none";
+    let a=parseInt(document.getElementById('update2').innerHTML);
+    let b=parseInt(final.innerHTML);
+    document.getElementById('final-value').innerHTML=b-a;
+    couponOff.innerHTML=final.innerHTML;
+}
+function nth3(){
+    document.getElementById('nth3').style.display="none";
+    let a=parseInt(document.getElementById('update3').innerHTML);
+    let b=parseInt(final.innerHTML);
+    document.getElementById('final-value').innerHTML=b-a;
+    couponOff.innerHTML=final.innerHTML;
+}
